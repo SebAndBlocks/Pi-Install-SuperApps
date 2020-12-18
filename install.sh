1fc120b4b281cd1b3a473c5e00d0c4b54db1ea4a
TurquoiseTNT <69688534+TurquoiseTNT@users.noreply.github.com>
2020-12-18 07:37:13 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt-get install minecraft-pi
+sudo apt-get install python-games
+sudo apt-get install steamlink
+sudo apt-get install kodi
+https://github.com/kodi-community-addons/plugin.audio.spotify.git
+sudo apt-get install cups
+sudo apt-get install hplip
+wget https://github.com/Botspot/pi-apps
+~/pi-apps/install
+wget https://code.headmelted.com/installers/apt.sh
+chmod +x apt.sh
+sudo ./apt.sh
+sudo apt-get install code-oss
